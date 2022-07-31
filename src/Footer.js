8ba850a9a6299bc3456741f3f27b61996320dffb
AybYrn <aybukeyaren--@outlook.com>
2022-07-31 16:44:00 +0300

    favicon

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+      <footer className="foot">
+        <h2>2022 Development. all right reserved</h2>
+      </footer>
+    );
+  }
---
+++ src/MainContent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function MainConent(){
+    return(
+      <div>
+        <h1>WHY REACT ?</h1>
+        <ol>
+          <li>cart</li>
+          <li>curt</li>
+          <li>zıvır</li>
+          <li>ıvır</li>
+        </ol>
+      </div>
+    );
+  }
