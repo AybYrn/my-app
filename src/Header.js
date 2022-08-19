65f11a91bcc62903d6b887a70a6b10d913d61297
AybYrn <aybukeyaren--@outlook.com>
2022-08-19 20:01:21 +0300

    Slider light and dark mode is done

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 
 export default function Header(props) {
   return (
-    <header className={props.lightMode ? "light" : ""}>
+    <header className={props.lightMode ? "light" : "header"}>
       <img src={logo} alt="Logo"></img>
       <nav>
         <ul>Home</ul>
@@ -11,12 +11,12 @@ export default function Header(props) {
         <ul>FAQ</ul>
       </nav>
       <div className="toggle--container">
-        <span>Light</span>
+        <span className={!props.lightMode ? "bold" : ""}>Dark</span>
         <label className="switch">
           <input type="checkbox"/>
           <span className="slider round" onClick={props.toggleDarkMode}></span>
         </label>
-        <span>Dark</span>
+        <span className={props.lightMode ? "bold" : ""}>Light</span>
       </div>
     </header>
   );
